c571eecc17283cbc397335148967591ff8bcc51b
Danil M <antonovdania482@gmail.com>
2025-06-10 20:23:17 +0300

    Added default values for env constants

--- config/config.py
+++ config/config.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SEARCH_QUERY=os.getenv("SEARCH_QUERY")
-SKIP_PERCENT=os.getenv("SKIP_PERCENT")
+SEARCH_QUERY=os.getenv("SEARCH_QUERY") or "You forgot to create SEARCH_QUERY param in .env :)"
+SKIP_PERCENT=os.getenv("SKIP_PERCENT") or "12"
